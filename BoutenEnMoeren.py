64eea2e13bdd614a253add9f8ea53a9ca0c50ca4
Earde Bearda <eardelb@hotmail.com>
2020-04-03 12:18:41 +0200

    bouten en moeren 2

--- BoutenEnMoeren.py
+++ BoutenEnMoeren.py
@@ -2,15 +2,17 @@ import numpy as np
 import cv2
 import math
 
-img = cv2.imread('./bouten_moeren.jpg')
+img = cv2.imread('./bouten_moeren2.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 grayBlur = cv2.GaussianBlur(gray, (25,25), 0)
 ret,th=cv2.threshold(grayBlur, 180, 255, cv2.THRESH_BINARY_INV)
 contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-factorRedelijkRond = 0.5
-factorErgRond = 0.8
-factorTeRond = 0.90
+thresholdRedelijkRond = 0.5
+thresholdErgRond = 0.8
+thresholdTeRond = 0.90
+
+thresholdVeelGaten = 50
 
 hierarchy = hierarchy[0]
 for cnr in range(len(contours)):
@@ -28,15 +30,15 @@ for cnr in range(len(contours)):
             holes += cv2.contourArea(contours[child])
             child = hierarchy[child][0]
         #klassificeer de bouten en moeren
-        if factor > factorTeRond:#binnenste cirkel van liggende moer (veel te rond)
+        if factor > thresholdTeRond:#binnenste cirkel van liggende moer (veel te rond)
             continue
-        elif factor < factorRedelijkRond and holes is 0: #liggende bout (niet rond, zonder gaten)
+        elif factor < thresholdRedelijkRond and holes is 0: #liggende bout (niet rond, zonder gaten)
             cv2.drawContours(img, [cnt], -1, (0,255,255), 3)
-        elif factor > factorErgRond and holes is 0: #staande bout (erg rond, zonder gaten)
+        elif factor > thresholdErgRond and holes < thresholdVeelGaten: #staande bout (erg rond, met weinig gaten)
             cv2.drawContours(img, [cnt], -1, (100,200,0), 3)
-        elif factor > factorRedelijkRond and holes > 0:#liggende moer(redelijk rond, met gaten)
+        elif factor > thresholdRedelijkRond and holes > thresholdVeelGaten:#liggende moer(redelijk rond, met veel gaten)
             cv2.drawContours(img, [cnt], -1, (255,0,0), 3)
-        elif factor > factorRedelijkRond and holes is 0:#staande moer(redelijk rond, zonder gaten)
+        elif factor > thresholdRedelijkRond and holes is 0:#staande moer(redelijk rond, zonder gaten)
             cv2.drawContours(img, [cnt], -1, (255,0,250), 3)
         print("Area: " + str(area) + " Factor: " + str(factor) + "Holes: " + str(holes))
 
